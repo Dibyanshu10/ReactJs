a3b86431dde15946306ea778366a0b650eed0f8f
Dibyanshu10 <dibyanshu.singh@resilientitservices.com>
2024-01-28 17:50:12 +0530

    Header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Hey there ! Learning react.js</div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <li>
+            <Link to={""}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
